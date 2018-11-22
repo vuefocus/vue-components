4ef90598ab0b5e4d17550c9b370573ac4f739e5d
Guillaume Duhan <guillaume.duhan@quatreepingles.fr>
2018-11-22 17:41:44 +0100

    Button

---
+++ components/Button.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+ <div class="Button">
+  <router-link v-if="type === 'link'" :to="{ name: link, params: {} }">
+   <button :class="className">
+    {{ contentButton }}
+   </button>
+  </router-link>
+  <button v-if="type === 'disabled'" :class="type">
+   {{ contentButton }}
+  </button>
+  <button v-if="type === 'onClick'" :class="className" @click="onClick">
+   {{ contentButton }}
+  </button>
+ </div>
+</template>
+
+<script>
+export default {
+    name: "Button",
+    props: {
+     className: String,
+     contentButton: String,
+     link: String,
+     onClick: {
+      type: Function
+     }
+     type: String
+    }
+}
+</script>
+
+<style lang="scss">
+.Button {}
+</style>
