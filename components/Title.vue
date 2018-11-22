e763b3effc69466ae19dad07201bcea965cff32b
Guillaume Duhan <guillaume.duhan@quatreepingles.fr>
2018-11-22 15:19:52 +0100

    Title

---
+++ components/Title.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+ <div class="title">
+  <h1 v-if="type === 'h1'" :class="className">{{ content }}</h1>
+  <h2 v-if="type === 'h2'" :class="className">{{ content }}</h2>
+  <h3 v-if="type === 'h3'" :class="className">{{ content }}</h3>
+  <p v-if="subtitle">{{ contentSub }}</p>
+ </div>
+</template>
+
+<script>
+export default {
+    name: "title",
+    props: {
+     contentTitle: String,
+     contentSub: String,
+     className: String,
+     subtitle: String,
+     type: String
+    }
+}
+</script>
+
+<style lang="scss">
+#title {}
+</style>
