40d0261e0305624bbbb99be3ba5954cba28c5c90
Guillaume Duhan <guillaume.duhan@quatreepingles.fr>
2018-12-28 17:27:24 +0100

    nav, loader and header update

---
+++ components/Loader.vue
@@ -0,0 +1,56 @@
+<template lang="html">
+  <div class="loader">
+   <p>Loader</p>
+  </div>
+</template>
+
+<script>
+export default {
+ name: "loader",
+ data() {
+  return {}
+ }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../assets/styles/colors.scss";
+
+.loader {
+    text-align: center;
+    margin: 15px 0;
+    width: 100%;
+
+    img {
+        animation-name: spin;
+        animation-duration: 1000ms;
+        animation-iteration-count: infinite;
+        animation-timing-function: linear;
+    }
+}
+
+@-moz-keyframes spin {
+    from {
+        -moz-transform: rotate(0deg);
+    }
+    to {
+        -moz-transform: rotate(360deg);
+    }
+}
+@-webkit-keyframes spin {
+    from {
+        -webkit-transform: rotate(0deg);
+    }
+    to {
+        -webkit-transform: rotate(360deg);
+    }
+}
+@keyframes spin {
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+}
+</style>
---
+++ components/Nav.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <nav :class="className">
+   <ul v-if="content">
+    <li v-for="el in content">
+     <router-link :to="{ name: el.link, params: {} }">
+      {{ el.title }}
+     </router-link>
+    </li>
+   </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+ name: "Navigation",
+ data() {
+  return {}
+ },
+ props: {
+  className: String,
+  content: Object
+ }
+}
+</script>
+
+<style lang="scss">
+nav {}
+</style>
