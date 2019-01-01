8672bd111317fb45c3e430b98f05750e444825e8
Guillaume Duhan <guillaume.duhan@quatreepingles.fr>
2019-01-01 15:49:01 +0100

    update Message

--- components/Message.vue
+++ components/Message.vue
@@ -1,5 +1,5 @@
 <template lang="html">
- <div class="Message">
+ <div class="Message" :class="className || content.className">
   <p v-for="el, index in content.paragraph" :key="index">{{ el }}</p>
  </div>
 </template>
@@ -17,5 +17,33 @@ export default {
 </script>
 
 <style lang="scss">
-.Message {}
+.Message {
+    max-width: 300px;
+    position: absolute;
+    bottom: 25px;
+    right: 25px;
+    color: white;
+    padding: 15px;
+    border-radius: 5px;
+    opacity: 0;
+    animation-name: slidein;
+    animation-duration: 6s;
+}
+
+@keyframes slidein {
+    0% {
+        opacity: 0;
+    }
+
+    15% {
+        opacity: 1;
+    }
+
+    85% {
+        opacity: 1;
+    }
+
+    100% {
+        opacity: 0;
+    }
 </style>
