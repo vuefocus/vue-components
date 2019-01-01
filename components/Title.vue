ba0b1e6b4b5b58aee0cd4a4718afb3a575de0777
Guillaume Duhan <guillaume.duhan@quatreepingles.fr>
2019-01-01 15:13:32 +0100

    Adding title

---
+++ components/Title.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+ <div class="Title">
+  <h1 v-if="type === 'h1' || content.type === 'h1'" :class="className || content.className">{{ contentTitle || content.title }}</h1>
+  <h2 v-if="type === 'h2' || content.type === 'h2'" :class="className || content.className">{{ contentTitle || content.title }}</h2>
+  <h3 v-if="type === 'h3' || content.type === 'h3'" :class="className || content.className">{{ contentTitle || content.title }}</h3>
+ </div>
+</template>
+
+<script>
+export default {
+ name: "Title",
+ props: {
+  content: Object,
+  contentTitle: String,
+  className: String,
+  type: String
+ }
+}
+</script>
+
+<style lang="scss">
+.Title {
+}
+</style>
