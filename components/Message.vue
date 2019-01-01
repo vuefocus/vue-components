73c2e0c2e2166241ec1c60c34f088bae63a0a16f
Guillaume Duhan <guillaume.duhan@quatreepingles.fr>
2019-01-01 15:26:40 +0100

    updating

---
+++ components/Message.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+ <div class="Message">
+  <p v-for="el, index in content.paragraph" :key="index">{{ el }}</p>
+ </div>
+</template>
+
+<script>
+export default {
+ name: "Message",
+ props: {
+  content: Object,
+  className: String,
+  title: String,
+  type: String
+ }
+}
+</script>
+
+<style lang="scss">
+.Message {}
+</style>
